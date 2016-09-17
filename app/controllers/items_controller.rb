7b82bc090c47ed21ecb25d22bfe54e3defd4daab
Ben E <benjamin.eng7@gmail.com>
2016-09-16 21:55:55 -0700

    create item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.all
   end
+
+  def create
+     @item = Item.new(item_params)
+     if @item.save
+       render json: @item
+     else
+       render json: @item.errors, status: :unprocessable_entity
+     end
+  end
+
+   private
+     def item_params
+       params.require(:item).permit(:title, :amount, :date)
+     end
 end
